db43374fcc3580a3547a43cf4e930b633798347c
the-best-developer <im.dylan.gosnell@gmail.com>
2019-08-16 22:13:56 -0400

    added routes for resources table

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const projectEndpoints = require('./router/project.js');
 const server = express();
 
 server.use(express.json());
-server.use('/api/projects', projectEndpoints);
+server.use('/api/', projectEndpoints);
 
 server.listen(4000, () => {
     console.log(`Running on port 4000`);
--- router/project.js
+++ router/project.js
@@ -2,11 +2,29 @@ const express = require('express');
 const router = express.Router();
 const db = require('../projectDb.js');
 
-router.get('/', async (req, res) => {
+// ##########
+//  Resource
+// ##########
+
+// ##### POST #####
+router.post('/resource', async (req, res) => {
+    const resourceData = req.body;
+
+    try {
+        const addedResource = await db('resource').insert(resourceData);
+        res.json( addedResource ? addedResource : null );
+    }
+    catch (err) {
+        
+    }
+});
+
+// ##### GET #####
+router.get('/resource', async (req, res) => {
     
     try {
-        const projects = await db('projects');
-        res.json(projects);
+        const resources = await db('resource');
+        res.json(resources);
     }
     catch (err) {
         res.status(500).json({ message: err.message });
